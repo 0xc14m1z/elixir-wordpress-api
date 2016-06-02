97f3adc8268f8030cee64e119ecb5ee621ab7cd1
Luca Ramundo <luca@ramundo.info>
2016-06-02 17:37:14 +0200

    Initialize mix application

---
+++ lib/elixir_wordpress_api.ex
@@ -0,0 +1,2 @@
+defmodule ElixirWordpressApi do
+end
---
+++ test/elixir_wordpress_api_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirWordpressApiTest do
+  use ExUnit.Case
+  doctest ElixirWordpressApi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
